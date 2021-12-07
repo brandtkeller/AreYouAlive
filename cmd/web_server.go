bca55ef5e1c93c1770c2d32e3dc6172a1363266f
brandtkeller <info@brandtkeller.net>
2021-12-07 05:13:54 +0000

    Feat: Initial structure for targets endpoint CRUD

--- cmd/web_server.go
+++ cmd/web_server.go
@@ -5,16 +5,38 @@ import (
 	"log"
 	"io"
 	"fmt"
+	"encoding/json"
 
     "github.com/gorilla/mux"
 )
 
 type target struct {
-	ID int `json:"id"`
+	Id string `json:"id"`
     Url string `json:"url"`
 	Interval int `json:"interval"`
 }
 
+var Targets []target
+
+// Database in-memory load
+func LoadData() {
+
+	Targets = append(Targets, target{Id: "1", Url: "test-one.com", Interval: 30})
+	Targets = append(Targets, target{Id: "2", Url: "test-two.com", Interval: 60})
+
+}
+
+
+// --------- Main response logic ----------
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+    response, _ := json.Marshal(payload)
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(code)
+    w.Write(response)
+}
+
+// ---------- Health Check Functions ----------
 func HealthCheckHandler(w http.ResponseWriter, req *http.Request) {
     // A very simple health check.
 	fmt.Printf(req.Method)
@@ -22,20 +44,41 @@ func HealthCheckHandler(w http.ResponseWriter, req *http.Request) {
     w.WriteHeader(http.StatusOK)
 
     // In the future we could report back on the status of our DB
-    // by performing a simple PING, and include them in the response.
     io.WriteString(w, `{"alive": true}`)
 }
 
+func GetAllTargetHandler( w http.ResponseWriter, req *http.Request ) {
+	fmt.Println("Endpoint Hit: returnAllTargets")
+    json.NewEncoder(w).Encode(Targets)
+}
+
+func getTargetById(w http.ResponseWriter, req *http.Request, key string ) {
+	fmt.Println("Endpoint Hit: returnAllTargets")
+
+	for _, target := range Targets {
+        if target.Id == key {
+            json.NewEncoder(w).Encode(target)
+        }
+    }
+
+
+}
+
+// ---------- Target Functions ----------
 func TargetHandler(w http.ResponseWriter, req *http.Request) {
+	
+	vars := mux.Vars(req)
+    key := vars["id"]
+	
 	switch req.Method {
-    case http.MethodPost:
-        // POST can only be on creation - single function required
+    // case http.MethodPost:
+    //     // POST can only be on creation - single function required
 	case http.MethodGet:
-        // Need to check for get all or get by ID
-    case http.MethodPut:
-        // PUT can be only by /target/{id} - single function required
-	case http.MethodDelete:
-		// DELETE can be only by /target/{id} - single functino required
+        getTargetById(w, req, key)
+    // case http.MethodPut:
+    //     // PUT can be only by /target/{id} - single function required
+	// case http.MethodDelete:
+	// 	// DELETE can be only by /target/{id} - single function required
 	default:
 		http.Error(w, "Bad request", http.StatusMethodNotAllowed)
 		return
@@ -44,13 +87,25 @@ func TargetHandler(w http.ResponseWriter, req *http.Request) {
 
 
 
+
 func main() {
 	router := mux.NewRouter()
 	// router.HandleFunc("/", rootHandler).Methods("GET")
-	//router.HandleFunc("/target", TargetHandler).Methods("POST", "GET", "PUT", "DELETE")
+
+	// ----- GET all targets -----
+	router.HandleFunc("/target", GetAllTargetHandler).Methods("GET")
+
+	// Do something with a specific Target
+	router.HandleFunc("/target/{id}", TargetHandler).Methods("POST", "GET", "PUT", "DELETE")
+	
 
 	router.HandleFunc("/health", HealthCheckHandler).Methods("GET")
-	fmt.Printf("Running the server on port 8080\n")
+	fmt.Println("Running the server on port 8080")
+	LoadData()
+
+	for i := 0; i < len(Targets); i++ {
+		fmt.Println(Targets[i].Url)
+	}
 
     log.Fatal(http.ListenAndServe("localhost:8080", router))
 }
